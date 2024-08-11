569fb64b97964898bc59a4c4f7ff739c66afb50b
axtant <70051483+axtant@users.noreply.github.com>
2024-08-11 10:58:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,65 @@
 class DashboardComponent {
-  constructor() {
-    this.message = '';
-
-    // Define your office premises boundaries (latitude and longitude)
-    this.officeBounds = {
-      north: 19.17729,
-      south: 19.17304,
-      east: 72.99136,
-      west: 72.98842
-    };
-  }
-
-  getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => this.showPosition(position),
-        (error) => this.showError(error)
+    constructor() {
+      this.messageElement = document.getElementById('demo');
+      this.message = '';
+      this.officeBounds = {
+        north: 19.17729,
+        south: 19.17304,
+        east: 72.99136,
+        west: 72.98842
+      };
+    }
+  
+    getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => this.showPosition(position),
+          (error) => this.showError(error)
+        );
+      } else {
+        this.messageElement.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+  
+    showPosition(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      this.message = `Latitude: ${latitude}<br>Longitude: ${longitude}`;
+      if (this.validateLocation(latitude, longitude)) {
+        this.message += "<br>You are within the office premises.";
+      } else {
+        this.message += "<br>You are not within the office premises.";
+      }
+      this.messageElement.innerHTML = this.message;
+    }
+  
+    validateLocation(lat, long) {
+      return (
+        lat <= this.officeBounds.north &&
+        lat >= this.officeBounds.south &&
+        long <= this.officeBounds.east &&
+        long >= this.officeBounds.west
       );
-    } else {
-      this.message = "Geolocation is not supported by this browser.";
+    }
+  
+    showError(error) {
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          this.messageElement.innerHTML = "User denied the request for Geolocation.";
+          break;
+        case error.POSITION_UNAVAILABLE:
+          this.messageElement.innerHTML = "Location information is unavailable.";
+          break;
+        case error.TIMEOUT:
+          this.messageElement.innerHTML = "The request to get user location timed out.";
+          break;
+        default:
+          this.messageElement.innerHTML = "An unknown error occurred.";
+          break;
+      }
     }
   }
-
-  showPosition(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    this.message = `Latitude: ${latitude}<br>Longitude: ${longitude}`;
-
-    if (this.validateLocation(latitude, longitude)) {
-      this.message += "<br>You are within the office premises.";
-    } else {
-      this.message += "<br>You are not within the office premises.";
-    }
-  }
-
-  validateLocation(lat, long) {
-    return (
-      lat <= this.officeBounds.north &&
-      lat >= this.officeBounds.south &&
-      long <= this.officeBounds.east &&
-      long >= this.officeBounds.west
-    );
-  }
-
-  showError(error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        this.message = "User denied the request for Geolocation.";
-        break;
-      case error.POSITION_UNAVAILABLE:
-        this.message = "Location information is unavailable.";
-        break;
-      case error.TIMEOUT:
-        this.message = "The request to get user location timed out.";
-        break;
-      default:
-        this.message = "An unknown error occurred.";
-        break;
-    }
-  }
-}
-
-// Create an instance of the component and call getLocation to initiate geolocation
-const dashboard = new DashboardComponent();
-dashboard.getLocation();
+  
+  const dashboard = new DashboardComponent();
+  document.getElementById('submitBtn').addEventListener('click', () => dashboard.getLocation());
+  
